b1c63536f2cd21541dea12ca2e94800c0afdd0e4
jun <ggprgrkjh@naver.com>
2024-01-29 01:42:13 +0900

    feat: spring-boot setting

---
+++ src/main/java/com/silverithm/vehicleplacementsystem/VehiclePlacementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.silverithm.vehicleplacementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclePlacementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclePlacementSystemApplication.class, args);
+	}
+
+}
