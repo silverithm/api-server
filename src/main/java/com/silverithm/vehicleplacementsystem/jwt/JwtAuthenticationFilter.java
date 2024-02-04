aa42da4e3bf49e7ff7a14426cc9b2faddfffdf7e
jun <ggprgrkjh@naver.com>
2024-02-05 03:03:05 +0900

    feat(WebSecurityConfig): add WebSecurityConfig

---
+++ src/main/java/com/silverithm/vehicleplacementsystem/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,17 @@
+package com.silverithm.vehicleplacementsystem.jwt;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+    }
+}
---
+++ src/main/java/com/silverithm/vehicleplacementsystem/security/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.silverithm.vehicleplacementsystem.security;
+
+import com.silverithm.vehicleplacementsystem.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfigure {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/h2-console/*").permitAll()
+                                .requestMatchers("/member/join").permitAll()
+                                .requestMatchers("/auth/login").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        //jwt filter 설정
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
