b92529b7b1d92de5b292cf0fc63f0970615ad507
jun <ggprgrkjh@naver.com>
2024-02-05 02:42:51 +0900

    feat(User): add User Entity

---
+++ src/main/java/com/silverithm/vehicleplacementsystem/entity/User.java
@@ -0,0 +1,33 @@
+package com.silverithm.vehicleplacementsystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<UserRole> userRoles;
+}
