1254a38ffa98dd1716aadbb2b74c307b19abdc16
jun <ggprgrkjh@naver.com>
2024-02-13 18:04:28 +0900

    feat(EmployeeController): add EmployeeController

---
+++ src/main/java/com/silverithm/vehicleplacementsystem/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.silverithm.vehicleplacementsystem.controller;
+
+
+import com.silverithm.vehicleplacementsystem.dto.AddElderRequest;
+import com.silverithm.vehicleplacementsystem.service.ElderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("/api/v1/employee")
+    public String elderAdd(@RequestBody AddEmployeeRequest addEmployeeRequest) {
+        employeeService.addEmployee(addEmployeeRequest);
+        return "Success";
+    }
+}
