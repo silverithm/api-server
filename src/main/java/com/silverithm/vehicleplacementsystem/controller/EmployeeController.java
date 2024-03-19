c66b646a7954bd13c741e1ca26370abbb20b0cbf
jun <ggprgrkjh@naver.com>
2024-03-19 11:53:38 +0900

    feat(EmployeeService): add getEmployees

--- src/main/java/com/silverithm/vehicleplacementsystem/controller/EmployeeController.java
+++ src/main/java/com/silverithm/vehicleplacementsystem/controller/EmployeeController.java
@@ -3,10 +3,18 @@ package com.silverithm.vehicleplacementsystem.controller;
 
 import com.silverithm.vehicleplacementsystem.dto.AddElderRequest;
 import com.silverithm.vehicleplacementsystem.dto.AddEmployeeRequest;
+import com.silverithm.vehicleplacementsystem.dto.ElderUpdateRequestDTO;
+import com.silverithm.vehicleplacementsystem.dto.ElderlyDTO;
+import com.silverithm.vehicleplacementsystem.dto.EmployeeDTO;
 import com.silverithm.vehicleplacementsystem.service.ElderService;
 import com.silverithm.vehicleplacementsystem.service.EmployeeService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +26,24 @@ public class EmployeeController {
     @PostMapping("/api/v1/employee")
     public String elderAdd(@RequestBody AddEmployeeRequest addEmployeeRequest) {
         employeeService.addEmployee(addEmployeeRequest);
+        return "Success";
+    }
+
+    @GetMapping("/api/v1/employees")
+    public List<EmployeeDTO> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @DeleteMapping("/api/v1/employee/{id}")
+    public String getElders(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
+        return "Success";
+    }
+
+    @PutMapping("/api/v1/employee/{id}")
+    public String updateElder(@PathVariable("id") final Long id,
+                              @RequestBody ElderUpdateRequestDTO elderUpdateRequestDTO) {
+        employeeService.updateEmployee(id, elderUpdateRequestDTO);
         return "Success";
     }
 }
--- src/main/java/com/silverithm/vehicleplacementsystem/service/EmployeeService.java
+++ src/main/java/com/silverithm/vehicleplacementsystem/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package com.silverithm.vehicleplacementsystem.service;
 
 import com.silverithm.vehicleplacementsystem.dto.AddEmployeeRequest;
+import com.silverithm.vehicleplacementsystem.dto.ElderUpdateRequestDTO;
+import com.silverithm.vehicleplacementsystem.dto.ElderlyDTO;
+import com.silverithm.vehicleplacementsystem.dto.EmployeeDTO;
 import com.silverithm.vehicleplacementsystem.dto.Location;
 import com.silverithm.vehicleplacementsystem.entity.Employee;
 import com.silverithm.vehicleplacementsystem.repository.EmployeeRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,13 +26,29 @@ public class EmployeeService {
         Location homeAddress = geocodingService.getAddressCoordinates(addEmployeeRequest.homeAddress());
         Location workPlace = geocodingService.getAddressCoordinates(addEmployeeRequest.workPlace());
 
-
-
         System.out.println(homeAddress + " " + addEmployeeRequest.homeAddress());
         System.out.println(workPlace + " " + addEmployeeRequest.workPlace());
 
         Employee employee = new Employee(addEmployeeRequest.name(), workPlace, homeAddress,
                 addEmployeeRequest.maxCapacity());
         employeeRepository.save(employee);
+    }
+
+    public List<EmployeeDTO> getEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employee -> new EmployeeDTO(employee.getId(), employee.getName(), employee.getHomeAddress(),
+                        employee.getWorkplace(),
+                        employee.getMaximumCapacity())).collect(Collectors.toList());
+
+        return employeeDTOS;
+
+    }
+
+    public void deleteEmployee(Long id) {
+    }
+
+    public void updateEmployee(Long id, ElderUpdateRequestDTO elderUpdateRequestDTO) {
     }
 }
