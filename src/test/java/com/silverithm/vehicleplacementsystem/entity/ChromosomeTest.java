6ab1dddc8f8efa72725561e5de904c9a08a854bf
jun <ggprgrkjh@naver.com>
2024-04-24 21:54:19 +0900

    test(ChromosomeTest): add BeforEach chromosome setup

--- src/test/java/com/silverithm/vehicleplacementsystem/entity/ChromosomeTest.java
+++ src/test/java/com/silverithm/vehicleplacementsystem/entity/ChromosomeTest.java
@@ -3,7 +3,10 @@ package com.silverithm.vehicleplacementsystem.entity;
 import java.util.List;
 import javax.swing.CellRendererPane;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Before;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,7 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 public class ChromosomeTest {
 
-    private Chromosome chromosome = new Chromosome();
+    private Chromosome chromosome;
+
+    @BeforeEach
+    public void setUp() {
+        chromosome = new Chromosome();
+    }
+
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
