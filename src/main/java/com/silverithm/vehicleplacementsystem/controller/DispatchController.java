f20f0a1395cf6472b8ef9b330f1324e2c5cb1e0c
jun <ggprgrkjh@naver.com>
2024-11-18 02:48:25 +0900

    feat(dispatch): dispatch cors 설정 추가

--- src/main/java/com/silverithm/vehicleplacementsystem/controller/DispatchController.java
+++ src/main/java/com/silverithm/vehicleplacementsystem/controller/DispatchController.java
@@ -12,14 +12,21 @@ import com.silverithm.vehicleplacementsystem.service.DispatchService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "https://silverithm.netlify.app",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+                RequestMethod.DELETE, RequestMethod.OPTIONS},
+        allowCredentials = "true")
 public class DispatchController {
 
     @Autowired
