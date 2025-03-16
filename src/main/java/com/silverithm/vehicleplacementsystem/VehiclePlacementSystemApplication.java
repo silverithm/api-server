912f37fe2d71a7b7a0c271f2f43194c47ba7b394
junhyeongkim2 <ggprgrkjh@naver.com>
2025-03-17 00:16:19 +0900

    feat(app): enablejpaauditing 추가

--- src/main/java/com/silverithm/vehicleplacementsystem/VehiclePlacementSystemApplication.java
+++ src/main/java/com/silverithm/vehicleplacementsystem/VehiclePlacementSystemApplication.java
@@ -4,12 +4,14 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableCaching
 @EnableScheduling
+@EnableJpaAuditing
 public class VehiclePlacementSystemApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(VehiclePlacementSystemApplication.class, args);
