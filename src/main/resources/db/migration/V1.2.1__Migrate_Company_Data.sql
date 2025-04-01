95ad15168ae947a15bae919121a6f5a2e5789c03
junhyeongkim2 <ggprgrkjh@naver.com>
2025-04-01 15:38:19 +0900

    feat(flyway): 고유 회사 데이터 추출 company 테이블 삽입 sql 주석 처리

--- src/main/resources/db/migration/V1.2.1__Migrate_Company_Data.sql
+++ src/main/resources/db/migration/V1.2.1__Migrate_Company_Data.sql
@@ -1,19 +1,19 @@
 -- V1.2.1__Migrate_Company_Data.sql
 -- 기존 사용자 데이터에서 고유한 회사 데이터 추출하여 company 테이블에 삽입
 
-INSERT INTO company (name, address_name, company_latitude, company_longitude, created_at, updated_at)
-SELECT DISTINCT
-    au.company_name,
-    au.company_address_name,
-    au.company_latitude,
-    au.company_longitude,
-    NOW(),
-    NOW()
-FROM app_user au
-WHERE au.company_name IS NOT NULL;
-
--- 사용자 레코드 업데이트하여 회사 ID 연결
-UPDATE app_user a
-    JOIN company c ON a.company_name = c.name
-    AND ((a.company_address_name = c.address_name) OR (a.company_address_name IS NULL AND c.address_name IS NULL))
-    SET a.company_id = c.id;
+-- INSERT INTO company (name, address_name, company_latitude, company_longitude, created_at, updated_at)
+-- SELECT DISTINCT
+--     au.company_name,
+--     au.company_address_name,
+--     au.company_latitude,
+--     au.company_longitude,
+--     NOW(),
+--     NOW()
+-- FROM app_user au
+-- WHERE au.company_name IS NOT NULL;
+--
+-- -- 사용자 레코드 업데이트하여 회사 ID 연결
+-- UPDATE app_user a
+--     JOIN company c ON a.company_name = c.name
+--     AND ((a.company_address_name = c.address_name) OR (a.company_address_name IS NULL AND c.address_name IS NULL))
+--     SET a.company_id = c.id;
